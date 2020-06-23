afea9c01da68b5d32ff781563e744f8fd20babcb
Michael K <michcio771@gmail.com>
2020-06-23 23:37:18 +0200

    Fixed autoload issue on linux

--- app/config/config.php
+++ app/config/config.php
@@ -3,5 +3,5 @@
  * Autoload needed classes
  */
 spl_autoload_register(function ($class) {
-    require_once(str_replace('\\', '/', $class . '.php'));
+    require_once(str_replace('\\', '/', strtolower($class) . '.php'));
 });
